4acf01b160c09b57f7e5002300f0a912bbcd3e40
Sven Skender <sskender@pm.me>
2022-04-23 22:10:11 +0200

    WIP dummy select queries

---
+++ 3-web-app/server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const sql = require('mssql')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const appPool = new sql.ConnectionPool(process.env.DB_CONN_STRING)
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', require('./routes'))
+app.use(require('./routes/errors').clientErrorHandler)
+app.use(require('./routes/errors').errorHandler)
+
+appPool.connect().then((pool) => {
+  app.locals.db = pool
+  const port = process.env.PORT || 3000
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+  })
+}).catch((err) => {
+  console.error('Error creating connection pool', err)
+})
