5a3bfeb2832be32cb993fb832e5bfeaf37b62a1f
Sven Skender <sskender@pm.me>
2022-05-01 02:22:41 +0200

    Simple data table

--- 3-web-app/client/src/App.js
+++ 3-web-app/client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import TableSelector from "./components/TableSelector";
 import TableTreeView from "./components/TableTreeView";
 import Editor from "./components/Editor";
+import DataTable from "./components/DataTable";
 import "./App.css";
 
 function App() {
@@ -13,32 +13,30 @@ function App() {
 
   return (
     <div className="App">
-      <Container>
-        <Grid container rowSpacing={1} columnSpacing={1}>
-          <Grid item xs={4}>
-            <Paper elevation={6} sx={{ mt: 4, padding: 2, paddingLeft: 4 }}>
-              <TableSelector
-                updateSelectedFactTable={setSelectedFactTable}
-              ></TableSelector>
-            </Paper>
-            <Paper elevation={6} sx={{ mt: 2, padding: 2 }}>
-              <TableTreeView
-                factTableId={selectedFactTable}
-                updateResults={setResults}
-              ></TableTreeView>
-            </Paper>
-          </Grid>
-
-          <Grid item xs={8}>
-            <Paper elevation={6} sx={{ mt: 4, ml: 2, padding: 2 }}>
-              <Editor sqlQuery={results.query} />
-            </Paper>
-            <Paper elevation={6} sx={{ mt: 4, ml: 2, padding: 4 }}>
-              data table
-            </Paper>
-          </Grid>
+      <Grid container rowSpacing={1} columnSpacing={1} sx={{ padding: 2 }}>
+        <Grid item xs={4}>
+          <Paper elevation={8} sx={{ mt: 4, padding: 2, paddingLeft: 4 }}>
+            <TableSelector
+              updateSelectedFactTable={setSelectedFactTable}
+            ></TableSelector>
+          </Paper>
+          <Paper elevation={8} sx={{ mt: 2, padding: 2 }}>
+            <TableTreeView
+              factTableId={selectedFactTable}
+              updateResults={setResults}
+            ></TableTreeView>
+          </Paper>
         </Grid>
-      </Container>
+
+        <Grid item xs={8}>
+          <Paper elevation={8} sx={{ mt: 4, ml: 2, padding: 2 }}>
+            <Editor sqlQuery={results.query} />
+          </Paper>
+          <Paper elevation={8} sx={{ mt: 4, ml: 2, padding: 1 }}>
+            <DataTable data={results.results}></DataTable>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ 3-web-app/client/src/components/DataTable.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function DataTable({ data }) {
+  const [header, setHeader] = React.useState([]);
+
+  React.useEffect(() => {
+    if (data === undefined || data === null) return;
+    if (data.length > 0) {
+      setHeader(Object.keys(data[0]));
+    }
+  }, [data]);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {header.map((title) => {
+              return <TableCell align="left">{title}</TableCell>;
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              {Object.keys(row).map((key) => {
+                return <TableCell align="left">{row[key]}</TableCell>;
+              })}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default DataTable;
